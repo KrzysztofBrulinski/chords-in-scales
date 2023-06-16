5fcea4eb5053da5943a5814cc3edd29b0d12d68d
Krzysztof <krzysztof.brulinski95@gmail.com>
2023-06-16 15:22:16 +0200

    Hello Kamil commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,9 @@
 	const { moll, dur } = scales || {};
 </script>
 
-<h1 class="text-[#f99f14] text-[36px] text-center">Welcome to SvelteKit Music Scales finder</h1>
+<h1 class="text-[#f99f14] text-[36px] text-center">
+	Cześć Kamil! :) Welcome to SvelteKit Music Scales finder
+</h1>
 
 <div class="mx-auto grid gap-8 sm:grid-cols-2 grid-cols-1 px-5">
 	<Scales title={'Dur'} sounds={dur} />
